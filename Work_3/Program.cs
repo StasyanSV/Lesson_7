bae8078d2de2c376ea4cc765e9eedc953af3aaf9
StasyanSV <stanislav_xx@mail.ru>
2022-10-28 17:58:35 +0400

    add Work_3

---
+++ Work_3/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+Console.Clear();
+
+Console.Write("Введите колличество строк: ");
+int lines = int.Parse(Console.ReadLine());
+
+Console.Write("Введите колличество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(lines, columns, 0, 9);
+PrintGetArray(array);
+squaringEvenArrayCoordinates(array);
+Console.WriteLine();
+PrintGetArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[n, m];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintGetArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] squaringEvenArrayCoordinates(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                inArray[i, j] = inArray[i, j] * inArray[i, j];
+            }
+        }
+    }
+
+    return inArray;
+}
